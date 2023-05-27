ebd93a80b37c68486298ea2c82f6e800b91ff80e
junny97 <pcohad12@gmail.com>
2023-05-28 01:17:19 +0900

    router로 shop과 store 페이지 분리

--- src/App.js
+++ src/App.js
@@ -8,40 +8,57 @@ import data from './data';
 import './App.css';
 import { useState } from 'react';
 import ListItem from './Components/ListItem';
+import Store from './Components/Store';
+import { Routes, Route, Link } from 'react-router-dom'
+
 function App() {
 
   let [shoes] = useState(data);
 
 
-
   return (
     <div className="App">
      
+    
+
      <Navbar bg="light" variant="light" style={{}}>
         <Container>
           <Navbar.Brand href="#home" style={{paddingRight:'1000px', fontWeight:'800'}}>KREAM</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home" style={{paddingRight:'30px'}}>shop</Nav.Link>
-            <Nav.Link href="#features" style={{paddingRight:'30px'}}>stores</Nav.Link>
+          <Link to="/"> <Nav.Link href="#home" style={{paddingRight:'30px'}}>shop</Nav.Link></Link>
+
+          <Link to="/store"> <Nav.Link href="#features" style={{paddingRight:'30px'}}>stores</Nav.Link></Link>
             <Nav.Link href="#pricing" style={{paddingRight:'30px'}}>login</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      
-      <div className='main-bg'>
-      </div>
-      <div className="container">
-    <div className="row">
-   {
-     shoes.map((a, index) => {
-      return(
-        <ListItem shoes={shoes[index]}  />
-      )
-     })
-   }
-    </div>
-  </div> 
 
+    {/* <Link to="/">홈</Link>
+    <Link to="/store">상세페이지</Link> */}
+
+      <Routes>
+      <Route path="/" element={
+        <>
+          <div className='main-bg'>
+          </div>
+          <div className="container">
+        <div className="row">
+       {
+        shoes.map((a, index)=> {
+         return(
+          <ListItem shoes={shoes[index]}/>
+         )
+        })
+       }
+        </div>
+      </div> 
+      </>
+       } />
+      <Route path="/store" element={ <Store /> } />
+      <Route path="/about" element={ <div>어바웃페이지임</div> } />
+    </Routes>
+      
+    
   
     </div>
   );
