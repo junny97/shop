21e9b3b735d774bdcc3bd51e0240a4851a494bbd
junny97 <pcohad12@gmail.com>
2023-06-03 00:52:28 +0900

    fetch API 으로 axios 요청

--- src/App.js
+++ src/App.js
@@ -1,86 +1,110 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import data from './data';
-import './App.css';
-import { useState } from 'react';
-import ListItem from './Components/ListItem';
-import Store from './Routes/Store';
-import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import data from "./data";
+import "./App.css";
+import { useState } from "react";
+import ListItem from "./Components/ListItem";
+import Store from "./Routes/Store";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+import axios from 'axios';
+
+
 
 function HeaderStyle() {
   //navigate
   let navigate = useNavigate();
-
   return (
     <Navbar bg="light" variant="light" style={{}}>
       <Container>
-        <Navbar.Brand href="#home" style={{ paddingRight: '1000px', fontWeight: '800' }}>
+        <Navbar.Brand
+          href="#home"
+          style={{ paddingRight: "1000px", fontWeight: "800" }}
+        >
           KREAM
         </Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link onClick={() => navigate('/')} style={{ paddingRight: '30px' }}>
+          <Nav.Link
+            onClick={() => navigate("/")}
+            style={{ paddingRight: "30px" }}
+          >
             Home
           </Nav.Link>
-          <Nav.Link onClick={() => navigate('/store')} style={{ paddingRight: '30px' }}>
+          <Nav.Link
+            onClick={() => navigate("/store")}
+            style={{ paddingRight: "30px" }}
+          >
             Stores
           </Nav.Link>
-          <Nav.Link onClick={() => navigate('/login')} style={{ paddingRight: '30px' }}>
+          <Nav.Link
+            onClick={() => navigate("/login")}
+            style={{ paddingRight: "30px" }}
+          >
             Login
-           
           </Nav.Link>
         </Nav>
       </Container>
-    
     </Navbar>
-    
   );
 }
 
-
-
 function App() {
-
   let [shoes] = useState(data);
+  let [moreShose, setMoreShose] = useState();
+
+  function fetchData() {
+    axios.get('https://codingapple1.github.io/shop/data2.json')
+    .then((result)=>{
+      setMoreShose(result.data)
+      
+    })
+    .catch(()=>{
+      console.log('데이터 전송에 실패했습니다')
+    });
+
+  }
+
 
   return (
     <div className="App">
-     
-     
-             <HeaderStyle />
-           
-    
-
-
+      <HeaderStyle />
       <Routes>
-      <Route path="/" element={
-        <>
-          <div className='main-bg'>
-          </div>
-          <div className="container">
-        <div className="row">
-       {
-        shoes.map((a, index)=> {
-         return(
-          <ListItem shoes={shoes[index]}/>
-         )
-        })
-       }
-        </div>
-      </div> 
-      </>
-       } />
-      <Route path="/store/:id" element={ <Store shoes={shoes}/> } />
-    </Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="main-bg"></div>
+              <div className="container">
+                <div className="row">
+                  {shoes.map((a, index) => {
+                    return <ListItem key={index} shoes={shoes[index]} />;
+                  })}
+                </div>
+              </div>
+              <button onClick={fetchData}>버튼</button>
+              {moreShose && (
+                <div className="container">
+                  <div className="row">
+                    {moreShose.map((a, index) => {
+                     return <ListItem key={index} shoes={a} />
+                     })}
+                  </div>
+                 </div>
+              )}
+            </>
+          }
+        />
+        {/* <Route path="/store/:id" element={<Store shoes={shoes} />}/>    */}
+        <Route path="/store/" element={<Store shoes={shoes}/>}>
       
-  
+        <Route path="/store/:id" element={<Store shoes={shoes} />}/>
+        </Route>
+      </Routes>
     </div>
   );
 }
-
-
 
 export default App;
--- src/Routes/Store.jsx
+++ src/Routes/Store.jsx
@@ -1,31 +1,33 @@
-import React from 'react'
+import { useEffect, useState } from 'react' 
 import { useParams } from 'react-router-dom'
 
-export default function Store({shoes}) {
+export default function Store({ shoes }) {
+  const [show, setShow] = useState(true);
+ 
 
-  let {id} = useParams();
-  let findId = shoes.find((x) =>{
-    return x.id == id;
-  })
-  
+
+
+  let { id } = useParams();
+  const findId = id ? shoes.find((x) => x.id == id) : shoes[0];
+
   return (
     <>
       <div className="container">
-  <div className="row">
-    <div className="col-md-6">
-    <img src={findId.img} width="100%" />
-    </div>
-    {
-      
-    }
-    <div className="col-md-6">
-      <h4 className="pt-5">{findId.title}</h4>
-      <p>{findId.content}</p>
-      <p>{findId.price}</p>
-      <button className="btn btn-danger">주문하기</button> 
-    </div>
-  </div>
-</div> 
+        <div className="row">
+          <div className="col-md-6">
+            <img src={findId.img} width="100%" />
+          </div>
+          <div className="col-md-6">
+            <h4 className="pt-5">{findId.title}</h4>
+            <p>{findId.content}</p>
+            <p>{findId.price}</p>
+            <button className="btn btn-danger">주문하기</button> 
+            
+          </div>
+        </div>
+      </div> 
     </>
   )
 }
+
+
