58a27e8c598ee4a0424b67787ab0932d13b44488
junny97 <pcohad12@gmail.com>
2023-06-08 00:20:55 +0900

    버튼 css 업데이트

--- src/Routes/Cart.jsx
+++ src/Routes/Cart.jsx
@@ -1,18 +1,26 @@
-import React from "react";
-import { Table } from "react-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
-import { increase, changeName } from "../store/userSlice";
-import {addCount,decreaseCount } from "../store";
+import React from 'react';
+import { Table } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { increase, changeName } from '../store/userSlice';
+import { addCount, decreaseCount } from '../store';
+import Button from 'react-bootstrap/Button';
 export default function Cart() {
   let state = useSelector((state) => state);
   let dispatch = useDispatch();
 
   return (
     <div>
-      <h6>{state.user.name} {state.user.age}의 장바구니</h6>
-      <button onClick={()=>{
-        dispatch(increase(10))
-      }}>버튼</button>
+      <h6>
+        {state.user.name} {state.user.age}의 장바구니
+      </h6>
+      <Button
+        variant='info'
+        onClick={() => {
+          dispatch(increase(10));
+        }}
+      >
+        정보
+      </Button>{' '}
       <Table>
         <thead>
           <tr>
@@ -22,23 +30,33 @@ export default function Cart() {
           </tr>
         </thead>
         <tbody>
-            {state.cart.map((a, i) => {
-              return (
-                <tr key={i}>
-                  <td>{a.id}</td>
-                  <td>{a.name}</td>
-                  <td>{a.count}</td>
-                  <td>
-                    <button onClick={()=>{
-                      dispatch(addCount(a.id))
-                    }}>+</button>
-                      <button onClick={()=>{
-                      dispatch(decreaseCount(a.id))
-                    }}>-</button>
-                  </td>
-                  </tr>
-              );
-            })}
+          {state.cart.map((a, i) => {
+            return (
+              <tr key={i}>
+                <td>{a.id}</td>
+                <td>{a.name}</td>
+                <td>{a.count}</td>
+                <td>
+                  <Button
+                    variant='success'
+                    onClick={() => {
+                      dispatch(addCount(a.id));
+                    }}
+                  >
+                    +
+                  </Button>{' '}
+                  <Button
+                    variant='danger'
+                    onClick={() => {
+                      dispatch(decreaseCount(a.id));
+                    }}
+                  >
+                    -
+                  </Button>{' '}
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </Table>
     </div>
