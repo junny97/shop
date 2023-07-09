a5899c2ada8941566f670b29007611c48e0e3136
junny97 <pcohad12@gmail.com>
2023-07-09 15:29:25 +0900

    navigarte로 상품 이미지 클릭시 스토어로 연결

--- src/Components/ListItem.jsx
+++ src/Components/ListItem.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-
-export default function ListItem({shoes}) {
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+export default function ListItem({ shoes }) {
+  const navigate = useNavigate();
   return (
-  
-      <div className="col-md-4">
-      <img src={shoes.img} width="80%" />
-        <h4>{shoes.title}</h4>
-        <p>{shoes.content}</p>
-      </div>
-  
-  )
+    <div
+      className='col-md-4'
+      onClick={() => navigate(`/store/${shoes.id}`)}
+      style={{ cursor: 'pointer' }}
+    >
+      <img src={shoes.img} width='80%' />
+      <h4>{shoes.title}</h4>
+      <p>{shoes.content}</p>
+    </div>
+  );
 }
